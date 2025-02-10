62a3e31b1906c394ec77f3c991cbf47546361817
Abhi-P22 <132135625+Abhi-P22@users.noreply.github.com>
2025-02-10 23:50:38 +0530

    Update useSocket.jsx

--- client/src/hooks/useSocket.jsx
+++ client/src/hooks/useSocket.jsx
@@ -1,7 +1,7 @@
 import { useEffect } from 'react';
 import { io } from 'socket.io-client';
 
-const socket = io('http://localhost:5000'); // Backend URL
+const socket = io('https://eventsync-p66k.onrender.com'); // Backend URL
 
 // const useSocket = (setAttendeeCount) => {
 //     useEffect(() => {
